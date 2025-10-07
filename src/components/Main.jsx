3b8c56b3c843aa9e7c522d2de171b44ba0af9a21
Ivan Bawar <ivanbawar@gmail.com>
2025-10-07 22:04:30 +0800

    add ingredients list and some useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
+import React, { useState } from "react";
+
 export const Main = () => {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    setItem = formData.get("ingredient");
+    ingredients.push(setItem);
+    console.log(ingredients);
+  };
+  const [item, setItem] = useState("");
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
-          aria-label="Add ingedient"
+          aria-label="Add ingredient"
           type="text"
           placeholder="e.g. oregano"
+          name="ingredient"
+          // value={ingredient}
         />
         <button>Add ingredient</button>
       </form>
+      <section className="ingredients-section">
+        <h1>Ingredients on hand:</h1>
+        <ul>
+          {ingredients.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </section>
     </main>
   );
 };
